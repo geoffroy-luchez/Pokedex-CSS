9975a4d2f82e70b195d4e809a7f3daeb07e25fcc
Geoffroy Luchez <Geoffroy.luchez@gmail.com>
2021-05-03 22:46:04 +0200

    Joystick terminé

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var pokedex = document.querySelector( '.pokedex' );
 var radioGroup = document.querySelector( '.radio-group' );
 var currentClass = '';
 
+var $ = window.jQuery;
+
 function changeSide() {
     var checkedRadio = radioGroup.querySelector( ':checked' );
     var showClass = 'show-' + checkedRadio.value;
@@ -11,7 +13,31 @@ function changeSide() {
     pokedex.classList.add( showClass );
     currentClass = showClass;
 }
-// set initial side
-//changeSide();
 
 radioGroup.addEventListener( 'change', changeSide );
+
+/* Joystick */
+/* Haut */
+$( ".joystick-cliquable__top" ).on( 'mousedown', function () {
+    $( ".joystick" ).addClass( 'joystick-angle-top' );
+} ).on( 'mouseup mouseleave', function () {
+    $( ".joystick" ).removeClass( 'joystick-angle-top' );
+} );
+/* Bas */
+$( ".joystick-cliquable__bottom" ).on( 'mousedown', function () {
+    $( ".joystick" ).addClass( 'joystick-angle-bottom' );
+} ).on( 'mouseup mouseleave', function () {
+    $( ".joystick" ).removeClass( 'joystick-angle-bottom' );
+} );
+/* Gauche */
+$( ".joystick-cliquable__left" ).on( 'mousedown', function () {
+    $( ".joystick" ).addClass( 'joystick-angle-left' );
+} ).on( 'mouseup mouseleave', function () {
+    $( ".joystick" ).removeClass( 'joystick-angle-left' );
+} );
+/* Droite */
+$( ".joystick-cliquable__right" ).on( 'mousedown', function () {
+    $( ".joystick" ).addClass( 'joystick-angle-right' );
+} ).on( 'mouseup mouseleave', function () {
+    $( ".joystick" ).removeClass( 'joystick-angle-right' );
+} );
