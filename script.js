5784a6d8334983e37031cc83185379f9c75d2954
Geoffroy Luchez <Geoffroy.luchez@gmail.com>
2021-05-06 17:33:27 +0200

    Avancée esthétique + sons

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 var $ = window.jQuery;
 
+/* https://youtu.be/4kRqai4ZbHA */
+var son_boot = new Audio( 'sons/boot.mp3' );
+var son_bouton = new Audio( 'sons/bouton.mp3' );
+/* https://www.youtube.com/watch?v=88qRmxhqoBA */
+var son_bip = new Audio( 'sons/bip.mp3' );
+/* https://youtu.be/PBOpSZja-1A */
+var son_extinction = new Audio( 'sons/extinction.mp3' );
+/* https://youtu.be/q_uze6Jnv6M */
+var son_demarrage = new Audio( 'sons/demarrage.mp3' );
+/* https://youtu.be/TgOm3ewdXcc */
+var son_probleme = new Audio( 'sons/probleme.mp3' );
+
 /* --------
    Joystick
    -------- */
 /* Haut */
-$( ".joystick-cliquable__top" ).on( 'mousedown', function () {
+$( ".joystick-cliquable--top" ).on( 'mousedown', function () {
     $( ".joystick" ).addClass( 'joystick-angle-top' );
 } ).on( 'mouseup mouseleave', function () {
     $( ".joystick" ).removeClass( 'joystick-angle-top' );
 } );
 /* Bas */
-$( ".joystick-cliquable__bottom" ).on( 'mousedown', function () {
+$( ".joystick-cliquable--bottom" ).on( 'mousedown', function () {
     $( ".joystick" ).addClass( 'joystick-angle-bottom' );
 } ).on( 'mouseup mouseleave', function () {
     $( ".joystick" ).removeClass( 'joystick-angle-bottom' );
 } );
 /* Gauche */
-$( ".joystick-cliquable__left" ).on( 'mousedown', function () {
+$( ".joystick-cliquable--left" ).on( 'mousedown', function () {
     $( ".joystick" ).addClass( 'joystick-angle-left' );
 } ).on( 'mouseup mouseleave', function () {
     $( ".joystick" ).removeClass( 'joystick-angle-left' );
 } );
 /* Droite */
-$( ".joystick-cliquable__right" ).on( 'mousedown', function () {
+$( ".joystick-cliquable--right" ).on( 'mousedown', function () {
     $( ".joystick" ).addClass( 'joystick-angle-right' );
 } ).on( 'mouseup mouseleave', function () {
     $( ".joystick" ).removeClass( 'joystick-angle-right' );
@@ -49,9 +61,47 @@ $( ".btn-noir" ).on( 'mousedown', function () {
    -------------------------------- */
 /* Fermture du Pokédex (clic sur le cadenas) */
 $( ".lock" ).on( 'click', function () {
-    $( ".pokedex" ).toggleClass( "ouvert" );
+    if ( $( ".pokedex" ).hasClass( "allume" ) ) {
+        setTimeout( function () {
+            son_extinction.play();
+            $( ".pokedex" ).removeClass( "ouvert" );
+            $( ".pokedex" ).removeClass( "allume" );
+        }, 500 );
+    }
 } );
+
 /* Ouverture du Pokédex (clic sur le clapet) */
 $( ".partie-secondaire__face--front" ).on( 'click', function () {
+    play_F( 'sons/boot.mp3' )
+    setTimeout( function () {
+        /* play_F( 'sons/demarrage.mp3' ); */
+        $( ".pokedex" ).addClass( "allume" );
+    }, 5500 );
     $( '.pokedex' ).toggleClass( "ouvert" );
 } );
+
+/* Son des touches */
+$( ".bouton-carre__face, .btn-noir__face" ).on( 'click', function () {
+    if ( $( ".pokedex" ).hasClass( "allume" ) ) {
+        play_F( 'sons/bip.mp3' );
+    }
+
+} );
+$( ".joystick-cliquable" ).on( 'click', function () {
+    if ( $( ".pokedex" ).hasClass( "allume" ) ) {
+        play_F( 'sons/pch.mp3' );
+    }
+} );
+
+/* Jouer sons (permet de jouer plusieurs son à la fois) */
+/* https://jsfiddle.net/pknsg809/2/ */
+function play_F( file ) {
+    var audio = document.createElement( 'audio' );
+    audio.src = file;
+    document.body.appendChild( audio );
+    audio.play();
+
+    audio.onended = function () {
+        this.parentNode.removeChild( this );
+    }
+}
