6e8d114da30c6122530329eb1e02a1ddc128a789
Geoffroy Luchez <Geoffroy.luchez@gmail.com>
2021-05-11 01:59:11 +0200

    Navigation dans le menu principal

--- script.js
+++ script.js
@@ -20,28 +20,14 @@ function play_F( file ) {
 }
 
 /* VUEJS */
-Vue.component( "ecran-principal__demarrage", { template: `
+/* Composant - Ecran principal */
+Vue.component( "ecran-principal__menu", {
+    props: ['menu'],
+    template: `
     <div>
-        <p>Pokedex terminal<br>
-        (v1.0.0)</p>
-        <div class="barre-chargement">
-            <div></div>
-        </div>
-        <p>Démarrage en cours...</p>
-    </div>
-` } );
-Vue.component( "ecran-principal__extinction", { template: `
-    <p>Extinction en cours...<p>
-` } );
-Vue.component( "ecran-principal__menu", { template: `
-    <div class='flex-center'>
-        <p>Menu princpal</o>
-    </div>
-` } );
 
-Vue.component( "ecran-secondaire__demarrage", { template: "<div>Démarrage en cours...</div>" } );
-Vue.component( "ecran-secondaire__extinction", { template: "<div>Extinction en cours...</div>" } );
-Vue.component( "ecran-secondaire__menu", { template: "<div>Menu princpal</div>" } );
+    </div>`
+} );
 
 var app = new Vue( {
     el: '.pokedex',
@@ -50,6 +36,10 @@ var app = new Vue( {
         boutons_bleus: [
             "111", "222", "333"
         ],
+        menu: [
+            "Tous les pokémons", "Pokémons par type", "Pokémons favoris", "Liste des baies"
+        ],
+        menu_actif: '0',
         ecran_secondaire: '',
         ecran_gauche: '',
         ecran_droit: '',
@@ -63,7 +53,7 @@ var app = new Vue( {
         },
         ecran_secondaire_actuel: function () {
             if ( this.etat != 'eteint' ) {
-                return "ecran-principal__" + this.etat;
+                return "ecran-secondaire__" + this.etat;
             }
         }
     },
@@ -114,7 +104,6 @@ var app = new Vue( {
                 /* Son et mouvements */
                 /* ( utilisation d'une fonction fléchée, sinon "this" ne pointe pas sur l'instance Vue mais sur la fonction annonyme ) */
                 setTimeout( () => {
-
                     this.etat = 'eteint';
 
                     play_F( 'sons/extinction.mp3' );
@@ -123,37 +112,100 @@ var app = new Vue( {
                 }, 500 );
             }
         },
+        /* Pression sur le bouton power */
+        power() {
+            if ( $( ".pokedex" ).hasClass( "ouvert" ) && $( ".pokedex" ).hasClass( "allume" ) && !$( ".pokedex" ).hasClass( "bouge" ) ) {
+                /* Si le Pokédex est ouvert et allumé */
+                this.etat = 'extinction';
+                $( ".pokedex" ).addClass( "bouge" )
+                /* Lumières */
+                setTimeout( function () {
+                    $( ".led-rouge" ).removeClass( 'allume' );
+                }, 0 );
+                setTimeout( function () {
+                    $( ".led-bleue" ).removeClass( 'allume' );
+                }, 500 );
+                /* Son et mouvements */
+                /* ( utilisation d'une fonction fléchée, sinon "this" ne pointe pas sur l'instance Vue mais sur la fonction annonyme ) */
+                setTimeout( () => {
+
+                    this.etat = 'eteint';
+
+                    play_F( 'sons/extinction.mp3' );
+                    $( ".pokedex" ).removeClass( "allume" );
+                    $( ".pokedex" ).removeClass( "bouge" );
+                }, 500 );
+
+            } else if ( $( ".pokedex" ).hasClass( "ouvert" ) && !$( ".pokedex" ).hasClass( "allume" ) && !$( ".pokedex" ).hasClass( "bouge" ) ) {
+                /* Si le Pokédex est ouvert et éteint */
+
+                this.etat = 'demarrage';
+
+                $( ".pokedex" ).addClass( "bouge" )
+                /* Son */
+                play_F( 'sons/boot.mp3' );
+                /* Lumières */
+                setTimeout( function () {
+                    $( ".led-rouge" ).addClass( 'allume' );
+                }, 2700 );
+                setTimeout( function () {
+                    $( ".led-bleue" ).addClass( 'allume' );
+                }, 5500 );
+                /* Mouvement */
+                /* ( utilisation d'une fonction fléchée, sinon "this" ne pointe pas sur l'instance Vue mais sur la fonction annonyme ) */
+                setTimeout( () => {
+
+                    this.etat = 'menu';
+
+                    /* Allumer le Pokedex */
+                    $( ".pokedex" ).addClass( "allume" );
+                    $( '.pokedex' ).removeClass( "bouge" );
+
+                }, 5500 );
+            }
+        },
         pression() {
             if ( $( ".pokedex" ).hasClass( "allume" ) ) {
                 play_F( 'sons/bip.mp3' );
             }
         },
         pression_joystick( param ) {
-            if ( param === 'top' ) {
-                $( ".joystick" ).addClass( 'joystick-angle-top' );
-                if ( $( ".pokedex" ).hasClass( "allume" ) ) {
-                    play_F( 'sons/pch.mp3' );
+            /* Rien ne marche pas si le Pokédex est éteint */
+            if ( $( ".pokedex" ).hasClass( "allume" ) ) {
+
+                if ( param === 'top' ) {
+                    /* Bouton haut */
+                    if ( this.menu_actif > 0 ) {
+                        play_F( 'sons/pch.mp3' );
+                        this.menu_actif--;
+                    } else {
+                        play_F( 'sons/probleme.mp3' );
+                    }
+                    $( ".joystick" ).addClass( 'joystick-angle-top' );
+                } else if ( param === 'bottom' ) {
+                    /* Bouton bas */
+                    if ( this.menu_actif < 3 ) {
+                        play_F( 'sons/pch.mp3' );
+                        this.menu_actif++;
+                    } else {
+                        play_F( 'sons/probleme.mp3' );
+                    }
+                    $( ".joystick" ).addClass( 'joystick-angle-bottom' );
+                } else if ( param === 'left' ) {
+                    /* Bouton gauche */
+                    $( ".joystick" ).addClass( 'joystick-angle-left' );
+                    play_F( 'sons/probleme.mp3' );
+                } else if ( param === 'right' ) {
+                    /* Bouton droite */
+                    $( ".joystick" ).addClass( 'joystick-angle-right' );
+                    play_F( 'sons/probleme.mp3' );
+                } else {
+                    /* Relachement ou sortie de bouton */
+                    $( ".joystick" ).removeClass( 'joystick-angle-top' );
+                    $( ".joystick" ).removeClass( 'joystick-angle-bottom' );
+                    $( ".joystick" ).removeClass( 'joystick-angle-left' );
+                    $( ".joystick" ).removeClass( 'joystick-angle-right' );
                 }
-            } else if ( param === 'bottom' ) {
-                $( ".joystick" ).addClass( 'joystick-angle-bottom' );
-                if ( $( ".pokedex" ).hasClass( "allume" ) ) {
-                    play_F( 'sons/pch.mp3' );
-                }
-            } else if ( param === 'left' ) {
-                $( ".joystick" ).addClass( 'joystick-angle-left' );
-                if ( $( ".pokedex" ).hasClass( "allume" ) ) {
-                    play_F( 'sons/pch.mp3' );
-                }
-            } else if ( param === 'right' ) {
-                $( ".joystick" ).addClass( 'joystick-angle-right' );
-                if ( $( ".pokedex" ).hasClass( "allume" ) ) {
-                    play_F( 'sons/pch.mp3' );
-                }
-            } else {
-                $( ".joystick" ).removeClass( 'joystick-angle-top' );
-                $( ".joystick" ).removeClass( 'joystick-angle-bottom' );
-                $( ".joystick" ).removeClass( 'joystick-angle-left' );
-                $( ".joystick" ).removeClass( 'joystick-angle-right' );
             }
         },
         pression_ok() {
